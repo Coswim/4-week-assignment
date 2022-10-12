3f0525e7694b67bb9806527d3f87415363528ef1
Coswim <jungem21@naver.com>
2022-10-13 00:16:15 +0900

    화면 가운데 정렬

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import CardList from "../components/CardList";
 import Form from "../components/Form";
 import Header from "../components/Header";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  max-width: 1200px;
+  min-width: 800px;
+  height: 100%;
+
+  margin: 0 auto;
+`;
 
 const Home = () => {
   return (
-    <div>
+    <Wrap>
       <Header />
       <Form />
       <CardList />
-    </div>
+    </Wrap>
   );
 };
 
